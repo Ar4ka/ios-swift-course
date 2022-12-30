04165244b002bb2217e6ad0a0dbe38657b046261
AR4KA <rene.juergens01@gmail.com>
2022-12-30 14:54:27 +0100

    started layout with code

---
+++ ObjectsWithCode/ObjectsWithCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ObjectsWithCode
+//
+//  Created by René Jürgens on 30.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
