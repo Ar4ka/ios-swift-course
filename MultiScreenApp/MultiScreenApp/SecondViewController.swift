ae2d1a3ea722177d649ab8777ef0a906b731db3d
AR4KA <rene.juergens01@gmail.com>
2023-01-03 23:20:58 +0100

    connected second view to its own controller

---
+++ MultiScreenApp/MultiScreenApp/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  MultiScreenApp
+//
+//  Created by René Jürgens on 03.01.23.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  @IBOutlet weak var myLabel: UILabel!
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
